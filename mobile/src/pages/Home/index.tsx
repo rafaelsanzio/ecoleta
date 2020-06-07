c3b211698fd3f24fc0e18e9aa92f2208463c8d7d
Rafael Sanzio <rafaelsanzio27@gmail.com.br>
2020-06-07 16:00:54 -0300

    Adding combo box for Uf and City on Home Screen
    Adding combo box for Uf and City on Home Screen

--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, ChangeEvent } from "react";
 import { Feather as Icon } from "@expo/vector-icons";
 import {
   View,
@@ -11,12 +11,61 @@ import {
   Platform,
 } from "react-native";
 import { RectButton } from "react-native-gesture-handler";
+import RNPickerSelect from "react-native-picker-select";
 import { useNavigation } from "@react-navigation/native";
+import axios from "axios";
+import { AppLoading } from "expo";
+
+interface IBGEUFResponse {
+  sigla: string;
+}
+interface IBGECityResponse {
+  nome: string;
+}
 
 const Home = () => {
-  const [uf, setUf] = useState("");
-  const [city, setCity] = useState("");
+  const [ufs, setUfs] = useState<string[]>([]);
+  const [cities, setCities] = useState<string[]>([]);
   const navigation = useNavigation();
+
+  const [uf, setUf] = useState("0");
+  const [city, setCity] = useState("0");
+
+  useEffect(() => {
+    axios
+      .get<IBGEUFResponse[]>(
+        "https://servicodados.ibge.gov.br/api/v1/localidades/estados"
+      )
+      .then((response) => {
+        const ufInitials = response.data.map((uf) => uf.sigla).sort();
+        setUfs(ufInitials);
+      });
+  }, []);
+  if (!ufs) {
+    return <AppLoading />;
+  }
+  useEffect(() => {
+    if (uf === "0") return;
+    axios
+      .get<IBGECityResponse[]>(
+        `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${uf}/municipios`
+      )
+      .then((response) => {
+        const cityNames = response.data.map((city) => city.nome).sort();
+        setCities(cityNames);
+      });
+  }, [uf]);
+
+  const placeholderUf = {
+    label: "Selecione o estado",
+    value: null,
+    color: "#9EA0A4",
+  };
+  const placeholderCity = {
+    label: "Selecione a cidade",
+    value: null,
+    color: "#9EA0A4",
+  };
 
   function handleNavigateToPoints() {
     navigation.navigate("Points", {
@@ -36,7 +85,9 @@ const Home = () => {
       >
         <View style={styles.main}>
           <Image source={require("../../assets/logo.png")} />
-          <Text style={styles.title}>Seu marketplace de cleta de resíduos</Text>
+          <Text style={styles.title}>
+            Seu marketplace de coleta de resíduos
+          </Text>
           <Text style={styles.description}>
             Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.
           </Text>
@@ -44,21 +95,25 @@ const Home = () => {
 
         <View style={styles.footer}>
           <View>
-            <TextInput
-              style={styles.input}
-              placeholder="Digite a UF"
+            <RNPickerSelect
+              onValueChange={(value) => setUf(value)}
               value={uf}
-              onChangeText={setUf}
-              maxLength={2}
-              autoCapitalize="characters"
-              autoCorrect={false}
+              style={pickerSelectStyles}
+              placeholder={placeholderUf}
+              items={ufs.map((uf) => {
+                return { label: uf, value: uf };
+              })}
+              useNativeAndroidPickerStyle={false}
             />
-            <TextInput
-              style={styles.input}
-              placeholder="Digite a Cidade"
+            <RNPickerSelect
+              onValueChange={(value) => setCity(value)}
               value={city}
-              onChangeText={setCity}
-              autoCorrect={false}
+              style={pickerSelectStyles}
+              placeholder={placeholderCity}
+              items={cities.map((uf) => {
+                return { label: uf, value: uf };
+              })}
+              useNativeAndroidPickerStyle={false}
             />
           </View>
           <RectButton style={styles.button} onPress={handleNavigateToPoints}>
@@ -141,6 +196,31 @@ const styles = StyleSheet.create({
     color: "#FFF",
     fontFamily: "Roboto_500Medium",
     fontSize: 16,
+  },
+});
+
+const pickerSelectStyles = StyleSheet.create({
+  inputIOS: {
+    height: 60,
+    fontSize: 16,
+    backgroundColor: "#FFF",
+    paddingVertical: 12,
+    marginBottom: 8,
+    paddingHorizontal: 24,
+    borderRadius: 10,
+    color: "#6C6C80",
+    paddingRight: 30, // to ensure the text is never behind the icon
+  },
+  inputAndroid: {
+    backgroundColor: "#FFF",
+    height: 60,
+    fontSize: 16,
+    paddingVertical: 12,
+    marginBottom: 8,
+    paddingHorizontal: 24,
+    borderRadius: 8,
+    color: "#6C6C80",
+    paddingRight: 30, // to ensure the text is never behind the icon
   },
 });
 
